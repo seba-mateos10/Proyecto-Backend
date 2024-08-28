b406d1a4cd9a4274a877022218c7e89fc7f173d8
seba-mateos10 <140267629+seba-mateos10@users.noreply.github.com>
2024-08-28 18:08:44 -0300

    funciones de PUT para agregar un producto al cart

---
+++ src/public/javascript/products.js
@@ -0,0 +1,23 @@
+const addProduct = async (id, cart) => {
+  const response = await fetch(
+    `http://localhost:8080/api/carts/${cart}/products/${id}`,
+    {
+      method: "PUT",
+    }
+  );
+
+  let responseJson = await response.json();
+
+  response.ok
+    ? Swal.fire({
+        icon: "success",
+        title: responseJson.message,
+        showConfirmButton: false,
+        timer: 1500,
+      })
+    : Swal.fire({
+        icon: "error",
+        title: responseJson.status,
+        text: responseJson.message,
+      });
+};
