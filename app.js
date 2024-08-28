44e2aa63c72e6e4426c098149c81d0a4f64e6905
seba-mateos10 <140267629+seba-mateos10@users.noreply.github.com>
2024-08-28 19:35:26 -0300

    proyecto final

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// const cluster          = require("node:cluster")
+// const { cpus }         = require("node:os")
 const { startServer } = require("./src/server.js");
-const { logger } = require("./src/utils/logger.js");
-const numeroDeCpu = cpus().length;
+// const { logger } = require("./src/utils/logger.js");
+// const numeroDeCpu = cpus().length;
 
 // if(cluster.isPrimary){
 //     logger.info("proceso primario")
