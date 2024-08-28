199a996db2201a5a5c087b8226c669f35bcb6e6e
seba-mateos10 <140267629+seba-mateos10@users.noreply.github.com>
2024-08-28 18:34:29 -0300

    agregamos logica para la ruta del carrito del usuario logeado

--- src/router/myProfileRouter.js
+++ src/router/myProfileRouter.js
@@ -1,34 +1,52 @@
 const { Router } = require("express");
 const { userService, cartService } = require("../service/services.js");
 const { passportCall } = require("../passportJwt/passportCall.js");
+const { authorization } = require("../passportJwt/authorization.js");
 
 const router = Router();
 
-router.get("/", passportCall("jwt"), async (req, res) => {
-  try {
-    const { email } = req.user;
-    const user = await userService.getUser({ email });
+router.get(
+  "/",
+  passportCall("jwt"),
+  authorization(["user", "premium"]),
+  async (req, res) => {
+    try {
+      const { email } = req.user;
+      const user = await userService.getUser({ email });
 
-    user
-      ? res.status(200).send({ status: "success", toInfo: user })
-      : res.status(404).send({
-          status: "Error",
-          message: "Your information does not exist",
-        });
-  } catch (error) {
-    console.log(error);
+      user
+        ? res.status(200).send({ status: "success", toInfo: user })
+        : res.status(404).send({
+            status: "Error",
+            message: "Your information does not exist",
+          });
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
-router.get("/myCart", passportCall("jwt"), async (req, res) => {
-  const { cart } = req.user;
-  const user = await userService.getUser({ cart });
-  const myCart = await cartService.getCartByID(user.cart._id);
+router.get(
+  "/myCart",
+  passportCall("jwt"),
+  authorization(["user", "premium"]),
+  async (req, res) => {
+    const { cart } = req.user;
+    const user = await userService.getUser({ cart });
+    const myCart = await cartService.getCartByID(user.cart._id);
 
-  myCart
-    ? res.send({ status: "Success", toCartIs: myCart })
-    : res.send({ status: "Error", message: "Cart not found" });
-});
+    const cartObj = {
+      title: "Cart",
+      style: "cart.css",
+      id: myCart._id,
+      products: myCart.products,
+    };
+
+    myCart
+      ? res.render("cartById", cartObj)
+      : res.send({ status: "Error", message: "Cart not found" });
+  }
+);
 router.get("/", (req, res) => {});
 
 module.exports = router;
