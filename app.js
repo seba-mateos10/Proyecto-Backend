4da7023a54b80f5925003ff54b28556f4eab2858
seba-mateos10 <140267629+seba-mateos10@users.noreply.github.com>
2024-08-28 19:08:06 -0300

    finalizando proyecto

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const cluster = require("node:cluster");
-const { cpus } = require("node:os");
 const { startServer } = require("./src/server.js");
 const { logger } = require("./src/utils/logger.js");
 const numeroDeCpu = cpus().length;
