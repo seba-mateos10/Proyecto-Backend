4553a1874bf66995f49455afdedb344399abf917
seba-mateos10 <140267629+seba-mateos10@users.noreply.github.com>
2024-09-04 11:11:19 -0300

    cambiamos la estructura de la card y la validacion

--- src/public/javascript/socket.js
+++ src/public/javascript/socket.js
@@ -4,19 +4,22 @@ socket.on("products", (data) => {
   let article = document.getElementById("listProducts");
   let products = "";
   data.forEach((product) => {
-    products += `<div class="card" style="width: 18rem; padding: 16px; margin: 20px">
-                        <h2 class="card-title">
-                            ${product.title} 
-                        </h2>
-                        <img src= ${product.thumbnails} class="card-img-top" style="width: 18rem; height: 12rem" alt=${this.title} />
-                        <h4 class="card-text">
-                            Price: ${product.price} usd 
-                        </h4>
-                        <div class="boxItem">
-                            <a class="icon-link" style="color:blue" href="">Ver mas...</a>
-                            <button id="boton" class="btnDelete"> Delete </button>
+    products += `<div class="card" style="width: 18rem; margin: 10px">
+                        <img src=${product.thumbnails} class="card-img-top" style="height: 11rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.title} </h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-                    </div>`;
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">description : ${product.description} </li>
+                            <li class="list-group-item">price : ${product.price} u$d</li>
+                            <li class="list-group-item">stock : ${product.stock} </li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div> `;
   });
   article.innerHTML = products;
 
@@ -31,26 +34,29 @@ socket.on("products", (data) => {
     let code = form.elements.code.value;
     let stock = form.elements.stock.value;
 
-    if (
-      title === "" ||
-      description === "" ||
-      price === "" ||
-      thumbnails === "" ||
-      code === "" ||
-      stock === ""
-    )
-      return console.log({
-        status: "Error",
-        mensaje: "Es obligatorio completar todos los campos",
-      });
-
-    socket.emit("addProduct", {
-      title,
-      description,
-      price,
-      thumbnails,
-      code,
-      stock,
-    });
+    title === "" ||
+    description === "" ||
+    price === "" ||
+    thumbnails === "" ||
+    code === "" ||
+    stock === ""
+      ? Swal.fire({
+          icon: "error",
+          title: "error",
+          text: "It is mandatory to complete all fields.",
+        })
+      : socket.emit("addProduct", {
+          title,
+          description,
+          price,
+          thumbnails,
+          code,
+          stock,
+        }) &&
+        Swal.fire({
+          icon: "success",
+          title: "success",
+          text: "the product has been created successfully",
+        });
   });
 });
