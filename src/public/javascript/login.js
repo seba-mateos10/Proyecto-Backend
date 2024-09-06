6b22e5ec95871a64891eb9383bd254a5534692f1
seba-mateos10 <140267629+seba-mateos10@users.noreply.github.com>
2024-09-06 02:18:44 -0300

    logica del formulario del login

--- src/public/javascript/login.js
+++ src/public/javascript/login.js
@@ -1,21 +1,37 @@
-console.log("Hola somos login");
-const form = document.getElementById("formCookie");
+const formLogin = document.getElementById("formLogin");
 
-form.addEventListener("submit", (event) => {
+formLogin.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  const data = new FormData(form);
-  const objectUser = {};
+  const data = new FormData(formLogin);
+  const userLogin = {};
 
-  data.forEach((value, key) => (objectUser[key] = value));
+  data.forEach((value, key) => (userLogin[key] = value));
 
-  fetch("/api/session/login", {
+  const response = await fetch("/api/session/login", {
     headers: {
       "Content-Type": "application/json",
     },
     method: "POST",
-    body: JSON.stringify(objectUser),
-  })
-    .then((res) => res.json())
-    .then((res) => localStorage.setItem("Codertoken", res.Accesstoken));
+    body: JSON.stringify(userLogin),
+  });
+
+  const responseJson = await response.json();
+
+  response.ok
+    ? Swal.fire({
+        icon: "success",
+        title: responseJson.message,
+        html: "<b>redirecting to the home page...</b>",
+        showConfirmButton: false,
+        timer: 1500,
+      }) &&
+      setTimeout(() => {
+        window.location.replace("/api/products");
+      }, 2000)
+    : Swal.fire({
+        icon: "error",
+        title: responseJson.status,
+        text: responseJson.message,
+      });
 });
